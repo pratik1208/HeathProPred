f85a6688c03df2ce65d46832d80cf09e2e463ed8
Pratik <pratik.marudwarmit@gmail.com>
2024-03-13 00:11:43 +0530

    Eighth commit

--- train.py
+++ train.py
@@ -26,14 +26,10 @@ dt = DecisionTreeClassifier()
 accuracy_score_test, accuracy_score_train = accuracy(
     dt, train_x_encoded, test_x_encoded, train_y, test_y
 )
-print(
-    f"(Test Accuracy : {accuracy_score_test: 4f} , Train accuracy : {accuracy_score_train: 4f})"
-)
 
 f1_score_test, f1_score_train = F1_score(
     dt, train_x_encoded, test_x_encoded, train_y, test_y
 )
-print(f"(Test F1 score : {f1_score_test: 4f}, Train F1 score : {f1_score_train:4f})")
 
 feature_importance(dt, train_x)
 
@@ -53,7 +49,7 @@ shuffle = True
 my_dataloader = DataLoader(dataset=my_dataset, batch_size=batch_size, shuffle=shuffle)
 device = torch.device("cuda")
 neuralnet = neuralnet.to(device)
-epochs = 1
+epochs = 20
 optimizer = torch.optim.Adam(neuralnet.parameters(), lr=0.001)
 loss = nn.BCELoss()
 
@@ -74,6 +70,43 @@ for epoch in tqdm(range(epochs)):
     train_epoch.append(epoch)
     train_accuracy.append(accuracy)
     train_f1_score.append(f1)
-    print(train_epoch)
-    print(train_accuracy)
-    print(train_f1_score)
+    print(
+        f"Train Accuracy at {epoch} : {accuracy} and F1 Score : {f1}"
+    )
+
+test_features = final_test
+test_target = test_y
+
+my_dataset_test = MyDataset(test_features, test_target)
+
+batch_size = 32
+shuffle = True
+my_dataloader_test = DataLoader(
+    dataset=my_dataset_test, batch_size=batch_size, shuffle=shuffle
+)
+
+evaluate(neuralnet, my_dataloader_test, device)
+
+optimizer = torch.optim.Adam(neuralnet.parameters(), lr=0.001)
+loss = nn.BCELoss()
+test_epoch = []
+test_accuracy = []
+test_f1_score = []
+epochs = 20
+for epoch in tqdm(range(epochs)):
+
+    for batch in tqdm(my_dataloader_test):
+        batch_features, batch_targets = batch
+        pred = neuralnet(batch_features.to(torch.float32).to(device))
+        l = loss(pred.squeeze(), batch_targets.to(torch.float32).to(device))
+        print(f"\rloss = {l.item()}", end=" ")
+        l.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    f1, accuracy = evaluate(neuralnet, my_dataloader_test, device)
+    test_epoch.append(epoch)
+    test_accuracy.append(accuracy)
+    test_f1_score.append(f1)
+    print(
+        f"Test Accuracy at {epoch} : {accuracy} and F1 Score : {f1}"
+    )
